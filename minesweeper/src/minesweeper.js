da3c88fbcd772f6c13c28413120266526e62c1a8
Timothy Yuen <theaznyune@gmail.com>
2018-02-28 00:02:43 -0500

    Finished second project part

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,11 +1,14 @@
-const blankLine = "   |   |   ";
-const guessLine = " 1 |   |   ";
-const bombLine = "   | B |   ";
-console.log("This is what an empty board would look like: ");
-for(i = 0; i < 3; i++){
-  console.log(blankLine);
-}
-console.log("This is what a board with a guess and a bomb on it would look like: ");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const printBoard = (inputBoard) => {
+  console.log("Current Board:");
+  inputBoard.forEach(function(item){
+    console.log(item.join(" | "));
+  });
+};
+let board = [[],[],[]];
+board.forEach(function(item){
+  item.push(" ", " ", " "); 
+});
+printBoard(board);
+board[0][1] = "1";
+board[2][2] = "B";
+printBoard(board);
